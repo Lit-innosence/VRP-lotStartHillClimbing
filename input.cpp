8b8144ad1f72a49973a226092b333adff05007d9
HaraaaOudoriigasuki <4622088@ed.tus.ac.jp>
2024-12-02 12:51:02 +0900

    input.cpp

--- input.cpp
+++ input.cpp
@@ -7,8 +7,8 @@ int filenum; //ファイル名の番号
 
 void input(){
     cout << "入力するファイル名について" << endl;
-    cout << "A or B ? :" << endl; cin >> AB;
-    cout << "番号は ? :" << endl; cin >> filenum;
+    cout << "A or B ?  :  "; cin >> AB;
+    cout << "番号は ?  :  "; cin >> filenum;
 
 
 }
