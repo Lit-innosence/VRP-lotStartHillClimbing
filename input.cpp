1d177f04f77634ae59951fc334d2dae3d4deb426
Lit-innosence <lit.azot@i.softbank.jp>
2024-12-02 12:55:51 +0900

    add valiants

--- input.cpp
+++ input.cpp
@@ -5,10 +5,15 @@ using namespace std;
 string AB; //ファイル名をAかBかを指定
 int filenum; //ファイル名の番号
 
+int n;  // 顧客数
+int q;  // トラックの積載量
+vector<int> coord_x, coord_y;   // 顧客のx座標，y座標
+vector<int> luggage;            // 顧客の荷物量
+
+vector<vector<int>> route;   // トラックの配送経路
+
 void input(){
     cout << "入力するファイル名について" << endl;
     cout << "A or B ? :" << endl; cin >> AB;
     cout << "番号は ? :" << endl; cin >> filenum;
-
-
 }
