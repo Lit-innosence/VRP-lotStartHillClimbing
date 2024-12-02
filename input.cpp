2bec03c526e75242dd511de3bf506e46db02fdf6
HaraaaOudoriigasuki <4622088@ed.tus.ac.jp>
2024-12-02 12:42:35 +0900

    input.cpp

---
+++ input.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+#include <fstream>
+using namespace std;
+
+string AB; //ファイル名をAかBかを指定
+int filenum; //ファイル名の番号
+
+void input(){
+    cin >> "入力するファイル名について" << endl;
+    cin >> "A or B ? :" << AB << endl;
+    cin >> "番号は ? :" << filenum << endl;
+
+
+}
