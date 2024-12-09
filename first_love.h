00894a71e2da455dae2ff8e6f964336bf4c32cf2
HaraaaOudoriigasuki <4622088@ed.tus.ac.jp>
2024-12-09 11:47:50 +0900

    first_love

---
+++ first_love.h
@@ -0,0 +1,63 @@
+#pragma once
+
+#include <bits/stdc++.h>
+#include <random>
+#include "variable.h"
+using namespace std;
+
+using vi = vector<int>;
+using vb = vector<bool>;
+using vvi = vector<vector<int>>;
+
+vi random_permulation(int n, int seed){
+    vi number(n+9);
+    vi permu(n+9);
+    vb appear_judge(n+9); //0,...,n-1をインデックスiとして，appear_judge.at(i)は既に登場したかどうかを判定する
+    vi seisuu(n+9);
+
+    mt19937 generator(seed); //メルセンヌツイスターで乱数生成
+
+    for(int i=1; i<=n; i++){
+        appear_judge.at(i) = false;
+        seisuu.at(i) = i;
+    }
+
+    for(int i=1; i<=n; i++){
+        uniform_int_distribution<int> distribution(1,n-i+1);
+        int val = distribution(generator);
+        //cout << "i: " << i << ", val: " << val << endl;
+        number.at(i) = val;
+    }
+    for(int i=1; i<=n; i++){
+        while(appear_judge.at(number.at(i))){ //number.at(i)番目の値が既に登場している場合
+            number.at(i)++;
+            //cout << "i: " << i << ", number.at(i): " << number.at(i) << endl;
+        } 
+        permu.at(i) = seisuu.at(number.at(i));
+        appear_judge.at(number.at(i)) = true;
+    }
+
+    return permu;
+}
+
+vvi first_solver(int n, int seed){
+    vi rp = random_permulation(n,seed);
+    vvi solver;
+    int t = 1;
+    while(t <= n){
+        vi track;
+        track.push_back(0);
+
+        int weight = 0;
+        do{
+            weight += luggage.at(rp.at(t));
+            track.push_back(rp.at(t));
+            t++;
+            
+        }while(t<=n && weight + luggage.at(rp.at(t)) <= q);
+        track.push_back(0);
+        solver.push_back(track);
+    }
+    
+    return solver;
+}
--- main.cpp
+++ main.cpp
@@ -3,7 +3,15 @@ using namespace std;
 #include "variable.h"
 #include "input.h"
 #include "TSP.h"
+#include "first_love.h"
 
+int n;  // 顧客数
+int q;  // トラックの積載量
+vector<int> coord_x, coord_y;   // 顧客のx座標，y座標
+vector<int> luggage;            // 顧客の荷物量
+
+vector<vector<int>> route;   // トラックの配送経路
+int cost;
 
 int main(){
     input();
@@ -12,15 +20,24 @@ int main(){
     i.resize(n);
     cout << quota.size() << endl;
 
-    int seed = 10;
+    int seed = 32467;
     /*
     cout << "初期解の乱数は？： "; cin >> seed;
     */
-    tsp(seed,n);
+    vi p = random_permulation(n,seed);
+    route = first_solver(n,seed);
 
     //初期解の列挙(デバッグ用)
-    for(int i=0; i<n; i++){
-        cout << quota.at(i) << endl;
+    for(int i=1; i<=n; i++){
+        cout << p.at(i) << " ";
+    }
+    cout << endl;
+
+    for(int i=0;i<route.size();i++){
+        for(int j=0;j<route.at(i).size();j++){
+            cout << route.at(i).at(j) << " ";
+        }
+        cout << endl;
     }
 
     return 0;
